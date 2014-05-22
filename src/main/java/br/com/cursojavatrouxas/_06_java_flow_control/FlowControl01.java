ae45688888e5a66b93e71726d9374972552d3b00
eros <eroskoller@gmail.com>
2014-05-22 10:10:03 -0300

    flow control

---
+++ src/main/java/br/com/cursojavatrouxas/_06_java_flow_control/FlowControl01.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.cursojavatrouxas._06_java_flow_control;
+
+/**
+ *
+ * @author eros
+ */
+public class FlowControl01 {
+
+    public static void main(String[] args) {
+        
+//            for(int i = 0 ; i < 10 ; i++){
+//                if(i == 5){
+//                    continue;
+//                }else if(i == 10){
+//                    break;
+//                }else{
+//                    System.out.println("valor de i : "+i);
+//                }
+//            }
+            
+//            int i = 0;
+//            for(System.out.println("isso acontece primeiro") ;  i < 10 ; System.out.println("isso acontece toda a interacao")){
+//                i ++;
+//            }
+            
+            System.out.println(">>>>>   Fim     <<<<<<");
+    }
+
+}
---
+++ src/main/java/br/com/cursojavatrouxas/_06_java_flow_control/FlowControl03.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.cursojavatrouxas._06_java_flow_control;
+
+/**
+ *
+ * @author eros
+ */
+public class FlowControl03 {
+
+    public static void main(String[] args) {
+        int i = 0;
+        do{
+            i ++;
+            System.out.println("i = "+i);
+        }while( i < 10);
+    }
+
+}
